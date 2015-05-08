334464f307daa4c03b1558046ab7b0c28e03fc45
Christian Deonier <christian.deonier@airbnb.com>
2015-05-08 14:44:45 -0700

    Update tests to reflect refactoring

--- library/src/test/java/deeplinkdispatch/DeepLinkRegistryTest.java
+++ library/src/test/java/deeplinkdispatch/DeepLinkRegistryTest.java
@@ -13,30 +13,30 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class DeepLinkRegistryTest {
 
   @Test public void testSingleParameter() {
-    String uriWithParams = "airbnb://test/{param1}";
+    String hostPath = "test/{param1}";
 
     DeepLinkRegistry registry = new DeepLinkRegistry(null);
-    registry.registerDeepLink(uriWithParams, DeepLinkEntry.Type.CLASS, "TestActivity", null);
+    registry.registerDeepLink(hostPath, DeepLinkEntry.Type.CLASS, "TestActivity", null);
 
-    String uri = "airbnb://test/12345";
-    DeepLinkEntry entry = registry.parseUri(uri);
+    String inputHostPath = "test/12345";
+    DeepLinkEntry entry = registry.parseUri(inputHostPath);
     Assert.assertNotNull(entry);
 
-    Map<String, String> parameters = entry.getParameters(uri);
+    Map<String, String> parameters = entry.getParameters(inputHostPath);
     assertThat(parameters.get("param1")).isEqualTo("12345");
   }
 
   @Test public void testTwoParameters() {
-    String uriWithParams = "airbnb://test/{param1}/{param2}";
+    String hostPath = "test/{param1}/{param2}";
 
     DeepLinkRegistry registry = new DeepLinkRegistry(null);
-    registry.registerDeepLink(uriWithParams, DeepLinkEntry.Type.CLASS, "TestActivity", null);
+    registry.registerDeepLink(hostPath, DeepLinkEntry.Type.CLASS, "TestActivity", null);
 
-    String uri = "airbnb://test/12345/alice";
-    DeepLinkEntry entry = registry.parseUri(uri);
+    String inputHostPath = "test/12345/alice";
+    DeepLinkEntry entry = registry.parseUri(inputHostPath);
     Assert.assertNotNull(entry);
 
-    Map<String, String> parameters = entry.getParameters(uri);
+    Map<String, String> parameters = entry.getParameters(inputHostPath);
     assertThat(parameters.get("param1")).isEqualTo("12345");
     assertThat(parameters.get("param2")).isEqualTo("alice");
   }
