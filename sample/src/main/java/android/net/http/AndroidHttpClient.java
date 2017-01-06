62181a413983b4eaf9d378195291c119729fc2e7
Manuel Garcia Urreta <Manuel.Garcia.X.Urreta.-ND@disney.com>
2017-01-06 14:46:44 -0800

    update robolectric to v3.2

--- sample/src/main/java/android/net/http/AndroidHttpClient.java
+++
@@ -1,8 +0,0 @@
-package android.net.http;
-
-/**
- * TODO(felipecsl) Remove this file once this issue is fixed
- * https://github.com/robolectric/robolectric/issues/1862
- */
-public class AndroidHttpClient {
-}
