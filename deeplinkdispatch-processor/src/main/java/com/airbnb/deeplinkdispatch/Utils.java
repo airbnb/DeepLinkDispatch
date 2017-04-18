2bfe5f94a2a8bf9b74de3612546faae6b1890aa7
Jingwei Hao <jhao@twitter.com>
2017-04-17 22:08:24 -0700

    Addressed review comments.

--- deeplinkdispatch-processor/src/main/java/com/airbnb/deeplinkdispatch/Utils.java
+++ deeplinkdispatch-processor/src/main/java/com/airbnb/deeplinkdispatch/Utils.java
@@ -1,7 +1,6 @@
 package com.airbnb.deeplinkdispatch;
 
 final class Utils {
-
   static String decapitalize(String str) {
     if (str != null && str.length() != 0) {
       if (str.length() > 1 && Character.isUpperCase(str.charAt(1))
