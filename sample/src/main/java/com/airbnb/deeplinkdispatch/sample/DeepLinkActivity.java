4b6ebaad6ea8d5cd989ade81c894b778312c16eb
Andreas Rossbacher <andreas.rossbacher@airbnb.com>
2019-11-19 17:17:45 -0800

    Added some tracing

--- sample/src/main/java/com/airbnb/deeplinkdispatch/sample/DeepLinkActivity.java
+++ sample/src/main/java/com/airbnb/deeplinkdispatch/sample/DeepLinkActivity.java
@@ -3,6 +3,9 @@ package com.airbnb.deeplinkdispatch.sample;
 import android.app.Activity;
 import android.os.Bundle;
 
+import android.os.Debug;
+import android.os.SystemClock;
+import android.util.Log;
 import com.airbnb.deeplinkdispatch.DeepLinkHandler;
 import com.airbnb.deeplinkdispatch.sample.library.LibraryDeepLinkModule;
 import com.airbnb.deeplinkdispatch.sample.library.LibraryDeepLinkModuleLoader;
@@ -11,9 +14,13 @@ import com.airbnb.deeplinkdispatch.sample.library.LibraryDeepLinkModuleLoader;
 public class DeepLinkActivity extends Activity {
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    Debug.startMethodTracing("deeplink.trace",90000000);
+    long before = SystemClock.elapsedRealtime();
     DeepLinkDelegate deepLinkDelegate = new DeepLinkDelegate(
         new SampleModuleLoader(), new LibraryDeepLinkModuleLoader());
     deepLinkDelegate.dispatchFrom(this);
+    Log.d("DeepLinkActivity", "DeeplinkTime: "+ (Long.toString(SystemClock.elapsedRealtime() -before))+"ms");
+    Debug.stopMethodTracing();
     finish();
   }
 }
