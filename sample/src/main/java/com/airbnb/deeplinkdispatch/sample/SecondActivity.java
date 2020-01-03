ceaa74dd0ce272c72c0385a36122edd86102cd7b
Andreas Rossbacher <andreas.rossbacher@udo.edu>
2020-01-03 10:55:36 -0800

    Added benchmark tests using the AndroidX benchmark library (#269)
    * Added benchmark tests using the AndroidX benchmark library
    
    - Moved ScaleTestActivity to it's own lib so it can be benchmarked (sample-benchmarkable-library)
    - Added benchmark tests fow newing the registry, matching different URIs in the index and creating an result (sample-benchmark)
    - Updated readme with baseline benchmark results on Pixel 2 running Android 10
    - Updated compile SDK etc.
    - Error message in DeeplinkResult is never null so made it non null
    - createResult is never retuning null so made the return non null
    - Renamed resources to avoid collisions
    
    * Enable benchmarks to taget api 29
    Some updates from code review.
    
    * Delete old Scale test activity

--- sample/src/main/java/com/airbnb/deeplinkdispatch/sample/SecondActivity.java
+++ sample/src/main/java/com/airbnb/deeplinkdispatch/sample/SecondActivity.java
@@ -17,7 +17,7 @@ public class SecondActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    setContentView(R.layout.sample_activity_main);
 
     if (getIntent().getBooleanExtra(DeepLink.IS_DEEP_LINK, false)) {
       Bundle parameters = getIntent().getExtras();
