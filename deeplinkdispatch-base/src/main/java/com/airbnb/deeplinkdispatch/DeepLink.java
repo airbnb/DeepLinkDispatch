62b5ac6f21d24a564f415ef0efc854d94ec5ebbc
Andreas Rossbacher <andreas.rossbacher@airbnb.com>
2021-03-18 09:31:57 -0700

    Make annotations RUNTIME visible to better work with latest Dexguard.

--- deeplinkdispatch-base/src/main/java/com/airbnb/deeplinkdispatch/DeepLink.java
+++ deeplinkdispatch-base/src/main/java/com/airbnb/deeplinkdispatch/DeepLink.java
@@ -27,7 +27,10 @@ import java.lang.annotation.Target;
  * </code></pre>
  */
 @Target({ ElementType.TYPE, ElementType.METHOD })
-@Retention(RetentionPolicy.CLASS)
+// When using tools like Dexguard we require these annotations to still be inside the .dex files
+// produced by D8 but because of this bug https://issuetracker.google.com/issues/168524920 they
+// are not so we need to mark them as RetentionPolicy.RUNTIME.
+@Retention(RetentionPolicy.RUNTIME)
 public @interface DeepLink {
   String IS_DEEP_LINK = "is_deep_link_flag";
   String URI = "deep_link_uri";
--- deeplinkdispatch-base/src/main/java/com/airbnb/deeplinkdispatch/DeepLinkSpec.java
+++ deeplinkdispatch-base/src/main/java/com/airbnb/deeplinkdispatch/DeepLinkSpec.java
@@ -39,7 +39,10 @@ import java.lang.annotation.Target;
  * </ul>
  */
 @Target({ ElementType.ANNOTATION_TYPE })
-@Retention(RetentionPolicy.CLASS)
+// When using tools like Dexguard we require these annotations to still be inside the .dex files
+// produced by D8 but because of this bug https://issuetracker.google.com/issues/168524920 they
+// are not so we need to mark them as RetentionPolicy.RUNTIME.
+@Retention(RetentionPolicy.RUNTIME)
 public @interface DeepLinkSpec {
   String[] prefix();
 }
