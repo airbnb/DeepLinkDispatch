1d86049041d27bce421b5af2da65a4185c44fae1
Andreas Rossbacher <andreas.rossbacher@airbnb.com>
2022-01-04 17:22:31 -0800

    compile error

--- deeplinkdispatch/src/main/java/com/airbnb/deeplinkdispatch/BaseDeepLinkDelegate.kt
+++ deeplinkdispatch/src/main/java/com/airbnb/deeplinkdispatch/BaseDeepLinkDelegate.kt
@@ -19,6 +19,7 @@ import java.lang.reflect.ParameterizedType
 import java.lang.reflect.Type
 
 open class BaseDeepLinkDelegate @JvmOverloads constructor(
+        bla
     val registries: List<BaseRegistry>,
     configurablePathSegmentReplacements: Map<String, String> = emptyMap(),
     private val typeConverters: () -> TypeConverters = { TypeConverters() },
