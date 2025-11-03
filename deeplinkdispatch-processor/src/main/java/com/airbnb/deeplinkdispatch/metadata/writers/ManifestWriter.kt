d4c1b39882bfb07ec0223aa0b160f1b17a2ec7a2
Andreas Rossbacher <andreas.rossbacher@airbnb.com>
2025-11-03 14:49:38 -0800

    Reformat

--- deeplinkdispatch-processor/src/main/java/com/airbnb/deeplinkdispatch/metadata/writers/ManifestWriter.kt
+++ deeplinkdispatch-processor/src/main/java/com/airbnb/deeplinkdispatch/metadata/writers/ManifestWriter.kt
@@ -27,59 +27,60 @@ internal class ManifestWriter : Writer {
                     // group by schemes and hosts as well.
                     println("        <activity")
                     println("            android:name=\"$activityClassFqn\" android:exported=\"true\">")
-                    elements.groupBy { deepLinkAnnotatedElement ->
-                        deepLinkAnnotatedElement.deepLinkUri.let { deepLinkUri ->
-                            Pair(
-                                deepLinkUri.scheme(),
-                                deepLinkUri.host()
-                            )
-                        }
-                    }.forEach { (schemeHostPair, elements) ->
-                        println("            <intent-filter>")
-                        println("                <action android:name=\"android.intent.action.VIEW\" />")
-                        println("                <category android:name=\"android.intent.category.DEFAULT\" />")
-                        println("                <category android:name=\"android.intent.category.BROWSABLE\" />")
-                        // There might be multiple URIs in a single intent filter, and we want to make sure there
-                        // are no duplicates (e.g. http and https for host over and over again)
-                        val allPossibleUrlValues =
-                            elements
-                                .map { element ->
-                                    val scheme = schemeHostPair.first
-                                    val host = schemeHostPair.second
-                                    val path = element.deepLinkUri.pathSegments().joinToString(prefix = "/", separator = "/")
-                                    UrlValues(
-                                        scheme.allPossibleValues().toSet(),
-                                        host.allPossibleValues().toSet(),
-                                        path.allPossibleValues().toSet(),
-                                    )
-                                }.reduce { acc, urlValues ->
-                                    UrlValues(
-                                        acc.schemeValues + urlValues.schemeValues,
-                                        acc.hostValues + urlValues.hostValues,
-                                        acc.pathValues + urlValues.pathValues,
-                                    )
-                                }
-                        allPossibleUrlValues.schemeValues.forEach { schemeValue ->
-                            println("                <data android:scheme=\"$schemeValue\" />")
-                        }
-                        allPossibleUrlValues.hostValues.forEach { hostValue ->
-                            println("                <data android:host=\"$hostValue\" />")
-                        }
-                        allPossibleUrlValues.pathValues.forEach { pathValue ->
-                            // If there is a simple glob pattern in the path, we need to use pathPattern instead of path
-                            // See: https://developer.android.com/guide/topics/manifest/data-element#path
-                            println(
-                                "                <data android:${
-                                    if (pathValue.contains(SIMPLE_GLOB_PATTERN)) {
-                                        "pathPattern"
-                                    } else {
-                                        "path"
+                    elements
+                        .groupBy { deepLinkAnnotatedElement ->
+                            deepLinkAnnotatedElement.deepLinkUri.let { deepLinkUri ->
+                                Pair(
+                                    deepLinkUri.scheme(),
+                                    deepLinkUri.host(),
+                                )
+                            }
+                        }.forEach { (schemeHostPair, elements) ->
+                            println("            <intent-filter>")
+                            println("                <action android:name=\"android.intent.action.VIEW\" />")
+                            println("                <category android:name=\"android.intent.category.DEFAULT\" />")
+                            println("                <category android:name=\"android.intent.category.BROWSABLE\" />")
+                            // There might be multiple URIs in a single intent filter, and we want to make sure there
+                            // are no duplicates (e.g. http and https for host over and over again)
+                            val allPossibleUrlValues =
+                                elements
+                                    .map { element ->
+                                        val scheme = schemeHostPair.first
+                                        val host = schemeHostPair.second
+                                        val path = element.deepLinkUri.pathSegments().joinToString(prefix = "/", separator = "/")
+                                        UrlValues(
+                                            scheme.allPossibleValues().toSet(),
+                                            host.allPossibleValues().toSet(),
+                                            path.allPossibleValues().toSet(),
+                                        )
+                                    }.reduce { acc, urlValues ->
+                                        UrlValues(
+                                            acc.schemeValues + urlValues.schemeValues,
+                                            acc.hostValues + urlValues.hostValues,
+                                            acc.pathValues + urlValues.pathValues,
+                                        )
                                     }
-                                }=\"$pathValue\" />",
-                            )
+                            allPossibleUrlValues.schemeValues.forEach { schemeValue ->
+                                println("                <data android:scheme=\"$schemeValue\" />")
+                            }
+                            allPossibleUrlValues.hostValues.forEach { hostValue ->
+                                println("                <data android:host=\"$hostValue\" />")
+                            }
+                            allPossibleUrlValues.pathValues.forEach { pathValue ->
+                                // If there is a simple glob pattern in the path, we need to use pathPattern instead of path
+                                // See: https://developer.android.com/guide/topics/manifest/data-element#path
+                                println(
+                                    "                <data android:${
+                                        if (pathValue.contains(SIMPLE_GLOB_PATTERN)) {
+                                            "pathPattern"
+                                        } else {
+                                            "path"
+                                        }
+                                    }=\"$pathValue\" />",
+                                )
+                            }
+                            println("            </intent-filter>")
                         }
-                        println("            </intent-filter>")
-                    }
                     println("        </activity>")
                 }
             println("    </application>")
